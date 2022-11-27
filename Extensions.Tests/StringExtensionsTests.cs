597db75797616b38a4a33c39990c5e689f31c497
Daniel St <danielstadnich@gmail.com>
2022-11-27 18:35:33 +0300

    StringExtensionsTests

--- Extensions.Tests/StringExtensionsTests.cs
+++ Extensions.Tests/StringExtensionsTests.cs
@@ -1,7 +1,20 @@
-﻿namespace Extensions.Tests;
+﻿using FluentAssertions;
+
+namespace Extensions.Tests;
 
 [TestFixture]
 public class StringExtensionsTests
 {
+    [Test]
+    public void IsNullOrEmpty_ShouldBe_Success()
+    {
+        string data1 = "123";
+        string? data2 = null;
+        string data3 = "";
+
+        data1.IsNullOrEmpty().Should().Be(string.IsNullOrEmpty(data1));
+        data2.IsNullOrEmpty().Should().Be(string.IsNullOrEmpty(data2));
+        data3.IsNullOrEmpty().Should().Be(string.IsNullOrEmpty(data3));
+    }
 }
 
